08b89b067558e4549e60fc3e44e11503af20a32a
Bessie <Bessie@NTINLTP2001474.FPS.NIHILENT.com>
2022-04-12 15:02:22 +0200

    Initial commit

---
+++ HelloWorld.py
@@ -0,0 +1,33 @@
+print("Hello world, I'm bessie")
+print(8 * "\n")
+print("end")
+f= "Bessiee"
+print(f)
+
+name = input("Please enter your name ")
+print("my name is", name)
+
+print("I am multiplying")
+num1 = int(input("Please enter your first number "))
+num2 = int(input("please enter your 2nd number "))
+
+print("Answer is", num1 * num2)
+
+print("I am dividing")
+num3 = int(input("pleae enter a number "))
+if num3 == 0:
+    print("number is 0")
+
+else:
+    print("Answer is", num2 / num3)
+
+print("Comparing numbers ")
+num5 = int(input("Please enter a number "))
+if num5> 0:
+    print("The number is greater than zero")
+
+elif num5<0:
+    print("The number is less than 0")
+
+else:
+    print("The number is zero")
