70e4bda591b2e4fb93c98cf37eb22ac0a2406da9
Bessie <thibedib.bt@gmail.com>
2022-04-13 13:08:13 +0200

    2nd commit

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,4 +1,4 @@
-print("Hello world, I'm bessie")
+"""print("Hello world, I'm bessie")
 print(8 * "\n")
 print("end")
 f= "Bessiee"
@@ -30,4 +30,31 @@ elif num5<0:
     print("The number is less than 0")
 
 else:
-    print("The number is zero")
+    print("The number is zero") """
+
+#Functions
+def Hello():
+    print("Hello")
+
+
+def GetInteger():
+    num = int(input("Enter an integer "))
+    return num
+
+def Cube(x):
+    num= int(input(x))
+    result = num**3
+    return result
+
+def Main():
+    print("Start")
+    Hello()
+    print( GetInteger())
+    print(Cube("Please enter a number "))
+
+    num7 = int(input("enter a number "))
+    for step in range (num7):
+        print(step)
+
+if __name__=="__main__":
+    Main()
